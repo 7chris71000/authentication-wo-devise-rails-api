f405c17e4487ff18d7fd465be5c0aa7f0aaa9788
Chris Francis <chris.francis@bitesite.ca>
2023-05-05 10:55:07 -0400

    Add rack attack

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,12 @@ class ApplicationController < ActionController::API
   attr_reader :current_user
 
   private
-    def authenticate_request
-      # request.headers is provided by rails
-      @current_user = AuthorizeApiRequest.call(request.headers).result
-      if !@current_user
-        render json: { error: 'Not Authorized' }, status: 401
-      end
+
+  def authenticate_request
+    # request.headers is provided by rails
+    @current_user = AuthorizeApiRequest.call(request.headers).result
+    if !@current_user
+      render json: { error: "Not Authorized" }, status: 401
     end
+  end
 end
