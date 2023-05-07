df2adb0d5354217e0d52991f941b43ec6a6e4b4d
Chris Francis <c.francis7@live.ca>
2023-05-07 18:17:18 -0400

    Update to ruby 3.2

--- app/commands/authenticate_user.rb
+++ app/commands/authenticate_user.rb
@@ -9,7 +9,7 @@ class AuthenticateUser
   end
 
   def call
-    # this is wehre the result gets returned
+    # this is where the result gets returned
     JsonWebToken.encode(user_id: user.id) if user
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2021_09_03_201224) do
+
   create_table "items", force: :cascade do |t|
     t.string "name"
     t.integer "price_cents"
@@ -26,4 +27,5 @@ ActiveRecord::Schema.define(version: 2021_09_03_201224) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
+
 end
